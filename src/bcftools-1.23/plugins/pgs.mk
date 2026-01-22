d89b1e8d3d485b8a06e646e5db9e2970b64c46fb
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-01-22 19:12:46 +0100

    try cholmod

--- src/bcftools-1.23/plugins/pgs.mk
+++ src/bcftools-1.23/plugins/pgs.mk
@@ -2,6 +2,8 @@
 # Only build if HAVE_CHOLMOD is yes
 ifeq ($(HAVE_CHOLMOD),yes)
 plugins/pgs.so: plugins/pgs.c
+	@echo "Building pgs plugin with CHOLMOD support"
+	@touch $@
 	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(EXTRA_CPPFLAGS) $(CHOLMOD_CPPFLAGS) $(LDFLAGS) -o $@ version.c $< $(PLUGIN_LIBS) $(LIBS) $(CHOLMOD_LIBS)
 else
 plugins/pgs.so:
